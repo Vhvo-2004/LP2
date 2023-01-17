71553e586f1cf20db8f099251af1b1f94603d42d
Vhvo-2004 <120145024+Vhvo-2004@users.noreply.github.com>
2023-01-17 15:05:36 -0400

    Update Prova_1-Questão1.c

--- Prova_1-Questão1.c
+++ Prova_1-Questão1.c
@@ -1,26 +1,30 @@
-
 #include <stdio.h>
 #include<math.h>
-float M,m,S,l,i;
  int main(){
+     float M,m,S,l,i;
      i=0;
-     
-     while(1){
+     l=0;
+     while(l>=0){
         scanf("%f",&l);  
         if(l<0){
-            break;
-        }
+		}else{
+		
+		
         if(i==0){
             M=l;
             m=l;
-        }if(M<l){
+        }
+		
+		if(M<l){
          M=l;
-     }if(m>l){
+        }
+		if(m>l){
          m=l;
+        }
+        S=S+l;
+        i++;}
      }
-     S=S+l;
-     i++;
-     }
+     
      printf("Maior Nota:%f\nMenor Nota:%f\nMedia aritmetica:%f\n",M,m,(S/i));
      return 0;
  }
