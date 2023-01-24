2e62931c25a347d77529b9bdc5ee367697728624
Vhvo-2004 <120145024+Vhvo-2004@users.noreply.github.com>
2023-01-24 16:19:32 -0400

    Add files via upload

---
+++ 20230124_005.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int verif(int a){
+	while(a<0||a>100){
+		scanf("%d",&a);
+	}
+	return a;
+}
+int fat(int fat){
+	int i,n;
+	n=1;
+	for(i=1;i<=fat;i++){
+		n=n*i;
+	}
+	return n;
+}
+void reverso(int n){
+	int C[100],i,V[100];
+	n=verif(n);
+	for(i=0;i<n;i++){
+		scanf("%d",&C[i]);
+	}
+	printf("Numero lido:");
+	for(i=0;i<n;i++){
+		printf("%d ",C[i]);
+	}
+		printf("\nNumero pares lido:");
+	for(i=0;i<n;i++){
+		V[i]=fat(C[i]);
+		printf("%d ",V[i]);
+	}
+
+}
+int n;
+int main(){
+	scanf("%d",&n);
+	reverso(n);
+	
+	return 0;
+}
