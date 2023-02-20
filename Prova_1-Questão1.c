18bcb5016635bf9a24cd568db3731dfb3b33a42a
Vhvo-2004 <120145024+Vhvo-2004@users.noreply.github.com>
2023-02-20 14:57:42 -0300

    Update Prova_1-Questão1.c

--- Prova_1-Questão1.c
+++ Prova_1-Questão1.c
@@ -28,36 +28,3 @@
      printf("Maior Nota:%f\nMenor Nota:%f\nMedia aritmetica:%f\n",M,m,(S/i));
      return 0;
  }
-#include<stdio.h>
-#include<string.h>
-char Nomes[2][100][10][56];
-void leitura (char Nomes[2][100][10][56]){
-	int i;
-	for(i=0;i<2;i++){
-		scanf("%s",Nomes[i][10][56]);
-	}
-	for(i=0;i<2;i++){
-		printf("Qual funcao do %s\n",Nomes[i][10][56]);
-		scanf("%s",Nomes[i][100][56]);
-		printf("\n");
-	}
-	for(i=0;i<2;i++){
-		printf("avaliacao do %s\n",Nomes[i][10][56]);
-		scanf("%s",Nomes[i][100][10]);
-		printf("\n");
-	}
-	for(i=0;i<2;i++){
-		printf("funcao do %s\n",Nomes[i][10][56]);
-		printf("%s\n",Nomes[i][100][56]);
-		printf("Avaliacao do %s: %s",Nomes[i][10][56],Nomes[i][100][10]);
-		printf("\n");
-	}
-	
-}
-int main(){
-
-leitura(Nomes);
-
-
-return 0;
-}
